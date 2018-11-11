6d09dc30734a6de44177f3267fdae2641de53293
constanceBN <constancebordes@gmail.com>
2018-11-11 16:22:37 +0100

    Initialisation du projet + diagrams V1

---
+++ src/main/java/co/simplon/boucheaoreille/BoucheAOreilleApplication.java
@@ -0,0 +1,12 @@
+package co.simplon.boucheaoreille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoucheAOreilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoucheAOreilleApplication.class, args);
+	}
+}
---
+++ src/test/java/co/simplon/boucheaoreille/BoucheAOreilleApplicationTests.java
@@ -0,0 +1,16 @@
+package co.simplon.boucheaoreille;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoucheAOreilleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
